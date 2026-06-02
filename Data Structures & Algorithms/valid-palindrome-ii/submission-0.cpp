d64eab5b41c2543b86a77524f0270d6ad93ac675
Srinjoy Aich <srinjoyaich4002@gmail.com>
2026-06-02 18:53:50 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindromeRange(string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] == s[right]) {
+                left++;
+                right--;
+            } else {
+                // Skip either left or right character
+                return isPalindromeRange(s, left + 1, right) || 
+                       isPalindromeRange(s, left, right - 1);
+            }
+        }
+        return true;
+    }
+};
