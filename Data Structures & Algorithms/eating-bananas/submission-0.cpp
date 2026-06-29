e81bd2d7bb60d90485710baea18b44fc6383bf28
Srinjoy Aich <srinjoyaich4002@gmail.com>
2026-06-29 12:25:18 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1; 
+        int high = *max_element(piles.begin(), piles.end()); 
+        int ans = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long hours = 0;
+
+            for (int pile : piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+
+            if (hours <= h) {
+                ans = mid;      
+                high = mid - 1;
+            } else {
+                low = mid + 1; 
+            }
+        }
+
+        return ans;
+    }
+};
