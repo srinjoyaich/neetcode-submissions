2d5cccf079057f2d77e40cb37d142025f180e234
Srinjoy Aich <srinjoyaich4002@gmail.com>
2026-09-01 13:57:05 +0530

    Add: online-stock-span - submission-1

---
+++ Data Structures & Algorithms/online-stock-span/submission-1.cpp
@@ -0,0 +1,22 @@
+class StockSpanner {
+    vector<int> arr;
+
+   public:
+    StockSpanner() {}
+
+    int next(int price) {
+        arr.push_back(price);
+        int i = arr.size() - 2;
+        while (i >= 0 && arr[i] <= price) {
+            i--;
+        }
+
+        return arr.size() - i - 1;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
